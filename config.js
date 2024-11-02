89ad15543beb5828d0be7b61c39cf064d038d783
xbigi <sdiljit122@gmail.com>
2024-11-02 12:41:40 +0100

    deploy

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkKNoQzwgu4uCAAVfz0fvzN6LxS1XIT44",
+    authDomain: "xbigi-xyz.firebaseapp.com",
+    projectId: "xbigi-xyz",
+    storageBucket: "xbigi-xyz.firebasestorage.app",
+    messagingSenderId: "144509631205",
+    appId: "1:144509631205:web:05833a87185f4c7ea0320c",
+    measurementId: "G-49T7BK2PJ1"
+};
+export {firebaseConfig};
