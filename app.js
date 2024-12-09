90aa412bb90357489b1f8716af385c7a6e16b3c1
xbigi <87718596+xbigi@users.noreply.github.com>
2024-12-09 13:38:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ document.getElementById('funnyButton').addEventListener('click', fetchLocation);
 
 window.addEventListener('load', async function() {
     const mainTitleEl = document.getElementById('mainTitle');
-    const defaultPhrase = "BALLA DO WHILE";
+    const defaultPhrase = "Welcome to xbigi.xyz";
 
     // Define a set of supported language codes for the API
     const supportedLanguages = new Set([
