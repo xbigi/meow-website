1d33a1ce8364fe03584e55dbb352dc781cf87445
xbigi <sdiljit122@gmail.com>
2025-02-28 22:35:42 +0100

    Updated site structure, added pages and assets

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("stars");
+if (canvas) {
+    const ctx = canvas.getContext("2d");
+
+    let stars = [];
+    const numStars = 100;
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas);
+
+    class Star {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+            this.radius = Math.random() * 1.5;
+            this.alpha = Math.random();
+            this.speed = Math.random() * 0.5 + 0.1;
+        }
+
+        draw() {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+            ctx.fill();
+        }
+
+        update() {
+            this.y += this.speed;
+            if (this.y > canvas.height) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+            this.alpha = Math.random();
+        }
+    }
+
+    function initStars() {
+        stars = [];
+        for (let i = 0; i < numStars; i++) {
+            stars.push(new Star());
+        }
+    }
+
+    function animateStars() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        stars.forEach(star => {
+            star.update();
+            star.draw();
+        });
+        requestAnimationFrame(animateStars);
+    }
+
+    initStars();
+    animateStars();
+}
